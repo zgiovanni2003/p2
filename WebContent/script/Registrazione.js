b4e5e9a834e6f52876adc558b2eebc432be339fe
zgiovanni2003 <zgiovanni2003@gmail.com>
2024-06-02 12:21:25 +0200

    id-4 resolved

--- WebContent/script/Registrazione.js
+++ WebContent/script/Registrazione.js
@@ -11,7 +11,7 @@ function checkNomeCognome(inputtxt) {
 
 
 function checkEmail(inputtxt) {
-	var email = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+	var email = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
 	if(inputtxt.value.match(email)) 
 		return true;
 	
